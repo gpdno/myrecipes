8eb78dcfce14ecc3b28ee302bb665b7a30b129ca
gpdno <gdeninno@suncoastde.com>
2017-02-27 20:05:10 -0500

    added new recipe form, added gravatar to show page and TTD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,5 +3,13 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
   
 end
