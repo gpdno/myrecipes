91b218abd64d1ad2a98cb9adcb277ae95f76a575
gpdno <gdeninno@suncoastde.com>
2017-03-05 11:42:10 -0500

    added full comment fuctionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get '/terms', to: 'pages#terms'
   get '/privacy', to: 'pages#privacy'
 
-  resources :recipes
+  resources :recipes do
+    resources :comments, only: [:create]
+  end
 
   get 'signup', to: 'chefs#new'
   resources :chefs, except: [:new]
