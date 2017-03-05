eb9d603091eebe1016ca939960399b718f429d65
gpdno <gdeninno@suncoastde.com>
2017-03-04 20:05:54 -0500

    added new and edit ingredients.  added ingredients to recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -48,7 +48,7 @@ end
 private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description)
+    params.require(:recipe).permit(:name, :description, ingredient_ids: [])
   end
 
   def set_recipe
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,8 @@
 class Ingredient < ApplicationRecord
+
   validates :name, presence: true, length: { minimum: 3, maximim: 25}
   validates_uniqueness_of :name
   has_many :recipe_ingredients
-  has_many :recipes, through: :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients, dependent: :destroy
 
 end
